5f1dd3afb47ef5b0e47c779c0faff6301811ee87
techerudite-akash-p <akash.patil@techerudite.com>
2024-10-23 14:40:39 +0530

    first-commit

---
+++ app/cabins/page.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { useState } from "react";
+
+export default function Page() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>Cabins Page</h1>
+            {count}
+            <button onClick={() => setCount((value) => value + 1)}>
+                Increase
+            </button>
+        </div>
+    );
+}
